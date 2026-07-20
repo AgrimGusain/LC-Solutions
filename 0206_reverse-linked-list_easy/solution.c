6a87384e631ad057fa83fbab18139344738319a2
Agrim Gusain <157783479+AgrimGusain@users.noreply.github.com>
2026-07-20 21:17:37 +0530

    Add Reverse Linked List solution

---
+++ 0206_reverse-linked-list_easy/solution.c
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == NULL || head->next == NULL) return head;
+        if(head->next->next == NULL){
+            ListNode* temp = head->next;
+            head->next->next = head;
+            head->next = NULL;
+            return temp;
+        }
+        ListNode* first = head;
+        ListNode* second = head->next;
+        ListNode* third = second->next;
+
+        while(third != NULL){
+            second->next = first;
+            first = second;
+            second = third;
+            third = third->next;
+        }
+        second->next = first;
+        head->next = NULL;
+        return second;
+
+    }
+};
