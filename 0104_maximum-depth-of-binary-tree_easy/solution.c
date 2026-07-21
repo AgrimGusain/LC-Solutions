1d4688a6a0c3b929356b3723d70cd304171ad62a
Agrim Gusain <157783479+AgrimGusain@users.noreply.github.com>
2026-07-21 10:24:32 +0530

    Add Maximum Depth Of Binary Tree solution

--- 0104_maximum-depth-of-binary-tree_easy/solution.c
+++ 0104_maximum-depth-of-binary-tree_easy/solution.c
@@ -1,13 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int func(TreeNode* r) {
-        if(r == nullptr) return 0; 
-        int l = func(r->left);
-        int ri = func(r->right);
-        return 1 + max(l, ri); 
-    }
-
     int maxDepth(TreeNode* root) {
-        return func(root);
+        if(root == NULL) return 0;
+        stack<pair<TreeNode* , int>> st;
+        st.push({root, 1});
+        int maxdepth = 0;
+
+        while(!st.empty()){
+            auto [curr, depth] = st.top();
+            st.pop();
+            maxdepth = max(maxdepth, depth);
+            if(curr->right) st.push({curr->right, 1+depth});
+            if(curr->left) st.push({curr->left, 1+depth});
+        }
+
+        return maxdepth;
+
     }
 };
