b1be2cce2de2c56069eaf381572e4afbfa3ddaca
Agrim Gusain <157783479+AgrimGusain@users.noreply.github.com>
2026-07-21 10:30:45 +0530

    Add Search In A Binary Search Tree solution

---
+++ 0700_search-in-a-binary-search-tree_easy/solution.c
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* func(TreeNode* r, int v){
+        if(r->val == v) return r;
+        else if(r->val < v) return func(r->left, v);
+        return func(r->right, v);
+    }
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root == NULL) return root;
+        TreeNode* ans = func(root, val);
+        return ans;
+    }
+};
